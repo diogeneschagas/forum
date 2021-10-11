235f9516b4c5a1b12be80dcba82ad6202cf60b75
diogenes.chagas <diogenes.chagas.terceirizado@mj.gov.br>
2021-10-10 21:58:52 -0300

    liberando acesso apenas ao método GET implementando segurança

---
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package br.com.alura.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	// Config autenticação (login, etc.)
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	}
+
+	// Config de Autorização
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+		.anyRequest().authenticated();
+	}
+
+	// Config de recursos estáticos (JS, CSS, imagens, etc)
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	}
+
+}
