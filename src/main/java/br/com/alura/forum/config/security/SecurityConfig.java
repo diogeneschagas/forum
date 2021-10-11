644ee2a4261a81bc736b68c8a6fdb1996c7950b9
diogenes.chagas <diogenes.chagas.terceirizado@mj.gov.br>
2021-10-10 23:42:10 -0300

    inserindo auteticacao do usuário

--- src/main/java/br/com/alura/forum/config/security/SecurityConfig.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,29 +8,41 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private AutenticacaoService autenticacaoService;
+
 	// Config autenticação (login, etc.)
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
 	}
 
 	// Config de Autorização
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
-		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
-		.anyRequest().authenticated() //indicar que outras URLs que não foram configuradas devem ter acesso restrito
-		.and().formLogin();
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll()
+				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll().anyRequest().authenticated() // indicar que
+																									// outras URLs que
+																									// não foram
+																									// configuradas
+																									// devem ter acesso
+																									// restrito
+				.and().formLogin();
 	}
 
 	// Config de recursos estáticos (JS, CSS, imagens, etc)
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 	}
-
+	
+	/* Gerador de HASH de SENHA
+	 * public static void main(String[] args) { System.out.println(new
+	 * BCryptPasswordEncoder().encode("123456")); }
+	 */
 }
