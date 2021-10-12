93c3823dbf421bbddabde4e34f817ec4b4682242
diogenes.chagas <diogenes.chagas.terceirizado@mj.gov.br>
2021-10-11 21:33:13 -0300

    alterações na camada de segurança e adição da dependência JWT

--- src/main/java/br/com/alura/forum/config/security/SecurityConfig.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
@@ -27,13 +28,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll()
-				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll().anyRequest().authenticated() // indicar que
+				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+				.antMatchers(HttpMethod.POST, "/auth").permitAll().anyRequest().authenticated() // indicar que
 																									// outras URLs que
 																									// não foram
 																									// configuradas
 																									// devem ter acesso
 																									// restrito
-				.and().formLogin();
+				.and().csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
 	// Config de recursos estáticos (JS, CSS, imagens, etc)
